22e50cc54142d202591f0e3d693565399ee5d88b
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-28 14:33:32 +0530

    setup of tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-green-400">
+   <h1 className="text-3xl font-bold text-green-800">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
