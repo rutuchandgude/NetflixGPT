67ee29ad00760433a59473e92193021973ed5d47
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-03-05 14:58:53 +0530

    sign out auth & updated profile

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,17 @@
 import { useState, useRef } from "react";
 import Header from "./Header";
 import { checkValidData } from "../utils/validate";
-import { createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
+import { createUserWithEmailAndPassword , signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const name = useRef(null);
   const email = useRef(null);
@@ -17,7 +20,7 @@ const Login = () => {
   const handleButtonClick = () => {
     //Validate the form data
     const message = checkValidData(
-     
+      // name.current.value,
       email.current.value,
       password.current.value
     );
@@ -34,10 +37,18 @@ const Login = () => {
         password.current.value
       )
         .then((userCredential) => {
-          // Signed up
           const user = userCredential.user;
+          updateProfile(user, {
+            displayName:name.current.value, photoURL: "https://avatars.githubusercontent.com/u/74643325?v=4"
+          }).then(() => {
+            const {uid, email, displayName, photoURL } = auth.currentUser;
+            dispatch(addUser({uid:uid, email:email,displayName:displayName, photoURL:photoURL}));
+            navigate("/browse");
+
+          }).catch((error) => {
+            setErrorMessage(error.message);
+          });
           console.log(user);
-          navigate("/browser");
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -52,7 +63,7 @@ signInWithEmailAndPassword(auth,  email.current.value,
     // Signed in 
     const user = userCredential.user;
     console.log(user);
-    navigate("/browser");
+    navigate("/browse");
 
   })
   .catch((error) => {
